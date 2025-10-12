ce1b2105a14b56b821cd749e7c6b92d36ba86eb2
Mohammed Ali <mohammeda2449556@gmail.com>
2025-10-12 03:10:29 +0100

    added lorentz

--- specialrel.py
+++ specialrel.py
@@ -1,1 +1,18 @@
 #this is my special relativity project I will update it as I go along
+#Packages
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+
+def lorentz_transformation(ct,x,v):
+
+    gamma = 1 / (np.sqrt(1-v**2))
+    x_prime = gamma * (x - v*ct)
+    t_prime = gamma * (ct - x*v )
+
+    return x_prime, t_prime 
+
+
+#now imagine a lamppost in some Frame S where it is stationary 
+
+#to create this already it becomes complicated what we need is a transformer (which we already have), a worldline generator, and finally a plotting machine showing the two frames.
