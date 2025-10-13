2d4820c5ef5ae158dea7cd5efc00c95b11906d02
Mohammed Ali <mohammeda2449556@gmail.com>
2025-10-13 23:37:40 +0100

    Length contraction

--- specialrel.py
+++ specialrel.py
@@ -22,10 +22,10 @@ t1 = np.linspace(0, 10, 100)
 x1 = np.full_like(t1, 2) # Creates an array of 2s, same size as t1
  
 # Now, transform these coordinates to get the worldline in Frame S'
-x_prime, t_prime = lorentz_transformation(t1, x1, 0.5)
+x_prime1, t_prime = lorentz_transformation(t1, x1, 0.5)
 
 
-fig, axs = plt.subplots(2, 1, figsize=(6, 8)) # 2 rows, 1 column
+fig, axs = plt.subplots(2, 1, figsize=(6, 8)) 
 
 # Plot for Frame S
 axs[0].plot(x1, t1)
@@ -37,14 +37,13 @@ axs[0].set_ylim(-1, 11)
 axs[0].grid(True)
 
 # Plot for Frame S'
-axs[1].plot(x_prime, t_prime)
+axs[1].plot(x_prime1, t_prime)
 axs[1].set_title("Frame S': Object appears to be moving")
 axs[1].set_xlabel("Space (x')")
 axs[1].set_ylabel("Time (ct')")
 axs[1].set_xlim(-1, 10)
 axs[1].set_ylim(-1, 11)
 axs[1].grid(True)
-
 
 plt.tight_layout()
 
